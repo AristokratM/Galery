438cdd65994157931192243a2e918863200dd593
AristokratM <61250102+AristokratM@users.noreply.github.com>
2020-11-04 16:04:12 +0200

    testing

--- pictures/urls.py
+++ pictures/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('picture/<int:pk>/', views.current_datetime, name="picture")
+    path('picture/<int:pk>/', views.current_datetime, name="picture"),
+    path('your-name/', views.get_name, name="your_name"),
 ]
--- pictures/views.py
+++ pictures/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from  django.shortcuts import render
 import datetime
+from .forms import NameForm
 # Create your views here.
 
 
@@ -7,3 +9,13 @@ def current_datetime(request, pk):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+
+def get_name(request):
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/picture/1/')
+    else:
+        form = NameForm()
+    return render(request, 'name.html', {'form': form})
